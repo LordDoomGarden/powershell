3ee4be0d5572ed7b25559e856d47016932616e59
mz <indyman_6661@hotmail.co.th>
2024-09-04 13:57:11 +0700

    add bak function

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,6 +1,13 @@
-#PSReadLine
+# In file ~/Documents/PowerShell/Microsoft.PowerShell_profile.ps1 
+# ."$env:USERPROFILE\.config\powershell\user_profile.ps1"
+
+# Module list
+# PSFzf
+# PSReadLine
+# z
+
+# PSReadLine
 Set-PSReadLineOption -EditMode Emacs
-# Set-PSReadLineOption -EditMode Vi
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord "Ctrl+d" -Function DeleteChar
 # Set-PSReadLineKeyHandler -Chord 'Ctrl+Spacebar' -Function MenuComplete
@@ -70,6 +77,66 @@ function ga {
 }
 function gg {
     git commit -m @Args
+}
+
+function .. {
+  cd ..
+}
+function ... {
+  cd ../..
+}
+
+function prc {
+  vim "$HOME/.config/powershell/user_profile.ps1" 
+}
+function urc {
+  vim "$HOME/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
+}
+
+
+function bak {
+param (
+    [string]$file
+  )
+
+  # Check if the file has a .bak extension
+  if ($file -match "\.bak$") {
+    # If the file is a backup (.bak), rename it back to the original file
+    $originalFile = $file -replace "\.bak$", ""
+    if (Test-Path $originalFile) {
+      # Prompt user for confirmation to replace the existing file
+      $response = Read-Host "File '$originalFile' already exists. Do you want to replace it? (Y/n)"
+      if ($response -eq "" -or $response -match "^[Yy]$") {
+        # Remove the existing original file before renaming
+        Remove-Item -Path $originalFile -Force
+        Rename-Item -Path $file -NewName $originalFile
+        Write-Host "Restored '$file' to '$originalFile'."
+      } else {
+        Write-Host "Operation canceled. '$originalFile' was not replaced."
+      }
+    } else {
+      Rename-Item -Path $file -NewName $originalFile
+      Write-Host "Restored '$file' to '$originalFile'."
+    }
+  } else {
+    # If the file is not a .bak, create a backup of the file
+    $backupFile = "$file.bak"
+    if (Test-Path $backupFile) {
+      # Prompt user for confirmation to replace the existing backup file
+      $response = Read-Host "Backup file '$backupFile' already exists. Do you want to replace it? (Y/n)"
+      if ($response -eq "" -or $response -match "^[Yy]$") {
+        # Remove the existing backup file before creating a new backup
+        Remove-Item -Path $backupFile -Force
+        Copy-Item -Path $file -Destination $backupFile
+        Write-Host "Backed up '$file' to '$backupFile' (replaced existing backup)."
+      } else {
+        Write-Host "Operation canceled. '$backupFile' was not replaced."
+      }
+    } else {
+      Copy-Item -Path $file -Destination $backupFile
+      Write-Host "Backed up '$file' to '$backupFile'."
+    }
+  }
 }
 
 # Prompt
